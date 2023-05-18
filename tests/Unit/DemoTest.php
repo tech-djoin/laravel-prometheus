ededc1ebc20ddf82524e1ffa838b6aad5becac71
Alit Indrawan <alitindrawan71@gmail.com>
2023-05-19 00:26:11 +0800

    feat(github-actions): Add test workflow

--- tests/Unit/DemoTest.php
+++ tests/Unit/DemoTest.php
@@ -3,7 +3,7 @@
 namespace TechDjoin\LaravelPrometheus\Tests\Unit;
 
 it('check the pest is work', function () {
-    dd(env('REDIS_HOST'), config('REDIS_PORT'));
+    dd(env('REDIS_HOST'), env('REDIS_PORT'));
 
     expect(true)->toBeTrue();
 });
